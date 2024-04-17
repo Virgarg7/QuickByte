e50f145507bb41cdbde10ca070b962b4e6e4583f
Vir garg <vir.garg.in@gmail.com>
2024-04-17 23:36:04 +0530

    App Download Component Completed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,21 +5,19 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 import Footer from '../../components/Footer/Footer';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 function Home() {
 
   const [category, setCategory] = useState("All");
 
   return (
-    <>
-      <div>
-        <Header />
-        <ExploreMenu category={category} setCategory={setCategory} />
-        <FoodDisplay category={category} />
-      </div>
-      <Footer />
-    </>
-
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
+    </div>
   )
 }
 
