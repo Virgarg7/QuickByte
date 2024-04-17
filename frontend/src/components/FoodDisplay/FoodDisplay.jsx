9c1b04f6c52fcaf68147fbf68f2b8fe074131d26
Vir garg <vir.garg.in@gmail.com>
2024-04-17 18:56:42 +0530

    Footer Completed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-cond-assign */
+/* eslint-disable no-constant-condition */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useContext } from 'react'
@@ -15,7 +17,9 @@ function FoodDisplay({ category }) {
             <div className='food-display-list'>
                 {
                     food_list.map((item, index) => {
-                        return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                        if (category === "All" || category === item.category) {
+                            return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                        }
                     })
                 }
             </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,17 +4,22 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import Footer from '../../components/Footer/Footer';
 
 function Home() {
 
   const [category, setCategory] = useState("All");
 
   return (
-    <div>
-      <Header />
-      <ExploreMenu category={category} setCategory={setCategory} />
-      <FoodDisplay category={category} />
-    </div>
+    <>
+      <div>
+        <Header />
+        <ExploreMenu category={category} setCategory={setCategory} />
+        <FoodDisplay category={category} />
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
