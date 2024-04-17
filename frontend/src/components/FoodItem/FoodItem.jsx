6edb585a80b1f5253d5522332e7cdefa9d2be2d5
Vir garg <vir.garg.in@gmail.com>
2024-04-17 18:12:50 +0530

    adding more code

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,15 +1,29 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext';
 
 function FoodItem({ id, name, price, description, image }) {
+    // Using the Context-api concept to implement addToCart and RemoveFromCart Functionlity 
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext)
+
     return (
         <div className='food-item'>
 
             <div className="food-item-img-container">
                 <img className="food-item-image" src={image} alt="" />
+                {!cartItems[id] ?
+                    <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+                    :
+                    <div className='food-item-counter'>
+                        <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+
+                    </div>
+                }
             </div>
 
             <div className="food-item-info">
